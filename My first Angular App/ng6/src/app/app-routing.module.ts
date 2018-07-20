7fa7963ea6a398e965978a41b296b234a88aae0f
anvesh25 <anveshbabu.26@gmail.com>
2018-07-21 00:35:13 +0530

    Added components in routing.module.ts file

--- My first Angular App/ng6/src/app/app-routing.module.ts
+++ My first Angular App/ng6/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UsersComponent} from './users/users.component';
+import {DetailsComponent} from './details/details.component';
+import {PostsComponent} from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // we define all our reouters here
+    path:'',
+    // this will tels us  which component will desided that path
+    component: UsersComponent
+  },
+  {
+    path:'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path:'posts',
+    component: PostsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
