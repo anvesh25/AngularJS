c14ed78682764b687d057f48a600e9db8726c1f3
anvesh25 <anveshbabu.26@gmail.com>
2018-07-24 00:11:55 +0530

    Started Quiz application learning

---
+++ My first Angular App/angular5/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
