eab66d61563bff7abb1ce7b3776fd745f9270828
anvesh25 <anveshbabu.26@gmail.com>
2018-07-23 23:44:40 +0530

    Adding menu items in menubar

--- My first Angular App/material-ng/src/app/material.ts
+++ My first Angular App/material-ng/src/app/material.ts
@@ -4,9 +4,10 @@ import { NgModule } from '../../node_modules/@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 //Importing material icon modules here and ading it as a input parameters
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 // To use @NgModule function please import '@angular/Core' as imported above
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-    exports:[MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule],
+    exports:[MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule],
 })
 export class MaterialModule{}
