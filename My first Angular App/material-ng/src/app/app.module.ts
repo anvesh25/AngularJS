86d7ca7e1ff1b66a5597403895048f8baff6da97
anvesh25 <anveshbabu.26@gmail.com>
2018-07-23 22:51:01 +0530

    Adding amimation and hammerjs modules to material application

---
+++ My first Angular App/material-ng/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Adding/importing browser animation modules
+import {BrowserAnimationsModule}from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
